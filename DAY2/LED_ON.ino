7dc69fc8b374a17c8ccdb5fa409da02ef5c61363
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-03-30 10:23:44 +0900

    digitwrite setup added

--- DAY2/LED_ON.ino
+++ DAY2/LED_ON.ino
@@ -1,12 +1,21 @@
-const int LED = 13;
+const int LED = 8;
 
 void setup(){
     pinMode(LED, OUTPUT);
+    digitalWrite(LED, HIGH);
+    Serial.begin(9600);
 }
 
 void loop(){
-    digitalWrite(LED, HIGH);
-    delay(100);
-    digitalWrite(LED, LOW);
-    delay(100);
+  int button = digitalRead(LED);
+    Serial.println(button);
+    if(button == 1){
+        digitalWrite(LED, LOW);
+        delay(100);
+       
+    }else{
+       digitalWrite(LED, HIGH);
+       delay(100);
+    }
+   
 }
