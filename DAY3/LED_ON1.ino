efce9763aa945a0fdce311a19e5f5457284810e8
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-03-31 15:29:31 +0900

    LED_ON button or not

---
+++ DAY3/LED_ON1.ino
@@ -0,0 +1,15 @@
+
+
+const int ledPin = 10;
+const int analogPin = A0;
+
+void setup(){
+	Serial.begin(9600);
+}
+
+void loop(){
+	int sensorInput = analogRead(analogPin);
+    Serial.println(sensorInput);
+    analogWrite(ledPin, sensorInput/4);
+    analogWrite(11,sensorInput/100);
+}
