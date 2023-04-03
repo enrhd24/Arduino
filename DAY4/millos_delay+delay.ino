f3095fe60849a0992875340aa0fa690f324a956d
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-04-03 10:53:57 +0900

    arduino time_now of delay1 and delay2

---
+++ DAY4/millos_delay+delay.ino
@@ -0,0 +1,24 @@
+unsigned long t1_prev = 0;
+const unsigned long t1_delay = 1000;
+
+unsigned long t2_prev = 0;
+const unsigned long t2_delay = 500;
+
+void setup(){
+  Serial.begin(115200);
+}
+void loop(){
+  
+  unsigned long t1_now = millis();
+  unsigned long t2_now = millis();
+  
+  if(t1_now - t1_prev >= t1_delay){
+    t1_prev = t1_now;
+    Serial.println(t1_now);
+  }
+  
+  if(t2_now - t2_prev >= t2_delay){
+    t2_prev = t2_now;
+    Serial.println(t2_now);
+  }
+}
