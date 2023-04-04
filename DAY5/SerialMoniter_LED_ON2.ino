ac4dbc734c2e0e85d06880a756ed1eb81ad33d2f
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-04-04 15:37:02 +0900

    Serial Moniter를 이용한 LED_ON Control2

---
+++ DAY5/SerialMoniter_LED_ON2.ino
@@ -0,0 +1,52 @@
+int arr[3] = {8,9,10};
+String Serial_Data = "";
+
+void setup(){
+  for(int i = 0; i < 3; i++){
+    pinMode(i,OUTPUT);
+  }
+  pinMode(7,INPUT);
+
+  Serial.begin(9600); // 통신속도로 설정
+  Serial.flush(); // 시리얼 상의 잔여 데이터가 남지않도록 초기화
+
+  // LED off
+  digitalWrite(arr[0], LOW);
+  digitalWrite(arr[1], LOW);
+  digitalWrite(arr[2], LOW);
+
+  Serial.println("해당 키를 입력해주세요");
+  Serial.println();
+}
+
+void loop(){
+
+   if (Serial.available() > 0)  // 시리얼 입력
+  {
+    Serial_Data = (char)Serial.read(); //한문자를 Serial_Data에 저장
+    Serial.print("Serial_Data : ");
+    Serial.println(Serial_Data);
+    
+    if (Serial_Data == "8") 
+    {
+      digitalWrite(arr[0], HIGH);
+      delay(500);
+      digitalWrite(arr[0], LOW);
+      delay(500);
+    }
+     else if (Serial_Data == "9") 
+    {
+      digitalWrite(arr[1], HIGH);
+      delay(500);
+      digitalWrite(arr[1], LOW);
+      delay(500);
+    }
+    else if (Serial_Data == "A")  
+    {
+      digitalWrite(arr[2], HIGH);
+      delay(500);
+      digitalWrite(arr[2], LOW);
+      delay(500);
+    }
+  }
+}
