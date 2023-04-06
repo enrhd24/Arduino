1357fab9650e9f318ece11e90da1cc1d2e5ebceb
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-04-06 16:08:04 +0900

    IR-Tracking added length - buzzer

---
+++ DAY6/LED_IR-Tracking .ino
@@ -0,0 +1,21 @@
+#define Read 7
+#define LED 2
+
+void setup(){
+  pinMode(Read, INPUT);
+  pinMode(LED,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  if(digitalRead(6) == 0){
+    digitalWrite(LED,HIGH);
+    delay(100);
+    Serial.println("흰색물체 감지");
+  }else{
+    digitalWrite(LED,LOW);
+    delay(100);
+    Serial.println("감지없음");
+  }
+  delay(10);
+}
