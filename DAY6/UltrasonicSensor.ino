5cb129b67d0acbc84c5c88338543cc929e4d77b2
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-04-06 10:00:04 +0900

    UltrasonicSensor.ino added

---
+++ DAY6/UltrasonicSensor.ino
@@ -0,0 +1,22 @@
+int trig = 2;                                   
+int echo = 3;                                
+
+void setup()
+{
+  Serial.begin(9600);                      
+  pinMode(trig, OUTPUT);                
+  pinMode(echo, INPUT);                 
+}
+
+void loop()
+{
+  digitalWrite(trig, HIGH);                                      
+  delayMicroseconds(10);                                      
+  digitalWrite(trig, LOW);                                       
+
+  int distance = pulseIn(echo, HIGH) * 17 / 1000;                   
+  Serial.print(distance);                                         
+  Serial.println("cm");                                           
+  delay(100); 
+}                                                    
+
