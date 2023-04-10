7f30e696a94cf5720406c247bd9ba55aaa2d116a
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-04-10 13:27:34 +0900

    cheat GPT of Problem solve

--- DAY7/LED_Digital_StateChangDet.ino
+++ DAY7/LED_Digital_StateChangDet.ino
@@ -1,44 +1,55 @@
-const int buttonPin = 2; 
-const int ledPin1 = 13;
-const int ledPin2 = 12;   
+const int BUTTON_PIN = 2;
+const int LED_PIN_1 = 13;
+const int LED_PIN_2 = 12;
 
-int buttonPushCounter = 0;  
-int buttonState = 0;        
-int lastButtonState = 0;   
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+int buttonPushCounter = 0;
+int buttonState = 0;
+int lastButtonState = 0;
+int ledState = LOW;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
 }
 
-
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState != lastButtonState) {
-    if (buttonState == HIGH) {
-      buttonPushCounter++;
-      Serial.println("on");
-      Serial.print("number of button pushes: ");
-      Serial.println(buttonPushCounter);
-    } else {
-      Serial.println("off");
-    }
-    delay(50);
-  }
-  lastButtonState = buttonState;
+  unsigned long currentTime = millis();
+  if (currentTime - lastDebounceTime >= debounceDelay) {
+    lastDebounceTime = currentTime;
 
+    buttonState = digitalRead(BUTTON_PIN);
+    if (buttonState != lastButtonState) {
+      lastButtonState = buttonState;
+
+      if (buttonState == HIGH) {
+        buttonPushCounter++;
+      }
+    }
+  }
 
   if (buttonPushCounter == 0) {
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, LOW);
+    if (ledState != LOW) {
+      digitalWrite(LED_PIN_1, LOW);
+      digitalWrite(LED_PIN_2, LOW);
+      ledState = LOW;
+    }
   } else if (buttonPushCounter % 2 == 0) {
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin1, LOW);
+    if (ledState != HIGH) {
+      digitalWrite(LED_PIN_2, HIGH);
+      digitalWrite(LED_PIN_1, LOW);
+      ledState = HIGH;
+    }
   } else if (buttonPushCounter % 2 != 0){
-    digitalWrite(ledPin2, LOW);
-    digitalWrite(ledPin1, HIGH);
-    
+    if (ledState != HIGH) {
+      digitalWrite(LED_PIN_2, LOW);
+      digitalWrite(LED_PIN_1, HIGH);
+      ledState = HIGH;
+    }
   }
+
+  digitalWrite(LED_PIN_1, ledState);
 }
