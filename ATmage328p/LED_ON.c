e9517777e8248ae91afc2a1fff846e826e0b4ef0
enrhd24 <enrhd567@gmail.com>
2023-04-20 17:06:51 +0900

    LED_ON added

---
+++ ATmage328p/LED_ON.c
@@ -0,0 +1,26 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void LED_lnit(void);
+void LED_On(unsigned char Data);
+
+int main(void){
+	LED_lnit();
+	for(;;){
+		LED_ON(0x01);
+		_delay_ms(100);
+		LED_ON(0x00);
+		_delay_ms(100);
+	}
+	
+	return 0;
+}
+
+void LED_lnit(void){
+	DDRB = 0x01;
+}
+
+void LED_ON(unsigned char Data){
+	PORTB = Data;
+}
+
